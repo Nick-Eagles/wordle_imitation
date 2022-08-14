7fd5588390f2f44e05d0baa9b783f6579f066e2f
Sculps <nickeagles77@gmail.com>
2022-08-13 23:32:38 -0400

    Add more packages to the installation script

--- installation/install.sh
+++ installation/install.sh
@@ -2,4 +2,11 @@
 #   When the project is further along, we'll provide an 'environment.yml' file
 #   and recommend creation of a conda environment.
 
+#   Create a basic environment with just python
 conda create -y -p $PWD/wordle_env python=3.8
+
+#   Add packages we'll need
+conda activate $PWD/wordle_env
+python -m pip install pandas
+python -m pip install openpyxl
+conda deactivate
