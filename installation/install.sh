ce486ab3545eab897aefc730ac340bea4426a062
Sculps <nickeagles77@gmail.com>
2022-08-13 20:58:40 -0400

    Add a script documenting the conda environment used for developing this project (later an 'environment.yml' file will be created)

---
+++ installation/install.sh
@@ -0,0 +1,5 @@
+#   Commands initially used to isolate python + dependencies while developing.
+#   When the project is further along, we'll provide an 'environment.yml' file
+#   and recommend creation of a conda environment.
+
+conda create -y -p $PWD/wordle_env python=3.8
