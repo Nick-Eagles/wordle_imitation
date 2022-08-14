32ffd943498f2bd0c9003e55b732996c20f4ba69
Sculps <nickeagles77@gmail.com>
2022-08-14 11:39:21 -0400

    Separate each square visually by adding a border; fix code to handle variable number of rows and columns

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 import pygame
+import numpy as np
 
 display_settings = {
     'square_size' : 100,
     'empty_color' : (255, 255, 255),
-    'miss_color' : (40, 40, 40),
+    'miss_color' : (140, 140, 140),
     'hit_color' : (0, 255, 0),
     'misplace_color' : (255, 255, 0),
-    'board_dimensions': (5, 5)
+    'border_color': (20, 20, 20),
+    'board_dimensions': (5, 7)
 }
 
 #   Given display settings, return (screen, board)
@@ -14,15 +16,16 @@ def start(display_settings):
     #   Initialize the display
     pygame.init()
     screen = pygame.display.set_mode((
-        display_settings['board_dimensions'][0] * display_settings['square_size'],
-        display_settings['board_dimensions'][1] * display_settings['square_size']
+        display_settings['board_dimensions'][1] * display_settings['square_size'],
+        display_settings['board_dimensions'][0] * display_settings['square_size']
     ))
-
+    #
     return (screen, np.full(display_settings['board_dimensions'], ''))
 
 #   Update the display given the current game board. So far, only handles square
 #   color-- you can't actually see any letters yet!
 def display_board(game_board, screen, display_settings, word):
+    #   Draw the squares containing each letter
     for row in range(display_settings['board_dimensions'][0]):
         for col in range(display_settings['board_dimensions'][1]):
             #   Color this square appropriately based on Wordle rules
@@ -34,16 +37,43 @@ def display_board(game_board, screen, display_settings, word):
                 color = display_settings['misplace_color']
             else:
                 color = display_settings['miss_color']
-
+            #
+            #   Draw this square
             pygame.draw.rect(
                 screen,
                 color,
                 pygame.Rect(
                     col * display_settings['square_size'],
                     row * display_settings['square_size'],
-                    display_settings['square_size'],
+                    display_settings['square_size'], 
                     display_settings['square_size']
                 )
             )
-
+    #
+    #   Draw horizontal lines to divide the squares visually
+    for row in range(1, display_settings['board_dimensions'][0]):
+        pygame.draw.line(
+            screen,
+            display_settings['border_color'],
+            (0, row * display_settings['square_size']),
+            (
+                display_settings['board_dimensions'][1] * display_settings['square_size'],
+                row * display_settings['square_size']
+            )
+        )
+    #
+    #   Draw vertical lines to divide the squares visually
+    for col in range(1, display_settings['board_dimensions'][1]):
+        pygame.draw.line(
+            screen,
+            display_settings['border_color'],
+            (col * display_settings['square_size'], 0),
+            (
+                col * display_settings['square_size'],
+                display_settings['square_size'] * display_settings['board_dimensions'][0]
+            )
+        )
     pygame.display.update()
+
+screen, board = start(display_settings)
+display_board(board, screen, display_settings, 'aaagffff')
