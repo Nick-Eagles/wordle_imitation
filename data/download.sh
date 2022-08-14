a72c076ba60d6b46598fa108435ca88a55f4babb
Sculps <nickeagles77@gmail.com>
2022-08-13 23:28:49 -0400

    Add a script to pull the word lists we'll use

---
+++ data/download.sh
@@ -0,0 +1,3 @@
+#   Pull the dataset we'll use: a pair of word lists with some overlap
+curl -O https://www.wordfrequency.info/samples/lemmas_60k.txt
+curl -O https://www.wordfrequency.info/samples/wordFrequency.xlsx
