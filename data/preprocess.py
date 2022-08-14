31192708504c87ec1643ac90bb8ab57f8bf9f76c
Sculps <nickeagles77@gmail.com>
2022-08-14 12:55:59 -0400

    Only filter out invalid words to form the cleaned list; later filter by length or frequency on demand

--- data/preprocess.py
+++ data/preprocess.py
@@ -2,39 +2,22 @@ import pandas as pd
 
 out_path = 'cleaned_word_list.csv'
 
-#   Manually determined cutoff, below which I have trouble recognizing many
-#   words
-FREQ_CUTOFF = 200
-
-WORD_LEN = 5
-
-#   Read in first list
+#   Read in first list and filter out words with special characters
 a = pd.read_csv('lemmas_60k.txt', skiprows = 7, sep = '\t')
 a['lemma'] = a['lemma'].astype(str)
+a = a[pd.Series(['-' not in x and "'" not in x for x in a['lemma']])]
 
-#   Grab valid words of the correct length
-good_indices = pd.Series(
-    ['-' not in x and len(x) == WORD_LEN for x in a['lemma']]
-)
-a = a[good_indices]
-
-#   Read in second list
+#   Read in second list and filter out words with special characters
 b = pd.read_excel('wordFrequency.xlsx', sheet_name = 1)
 b['lemma'] = b['lemma'].astype(str)
-
-#   Grab valid words of the correct length
-good_indices = pd.Series(
-    ['-' not in x and len(x) == WORD_LEN for x in b['lemma']]
-)
-b = b[good_indices]
+b = b[pd.Series(['-' not in x and "'" not in x for x in b['lemma']])]
 
 #   Now combine the lists but make sure words are unique
 a = a[~ a['lemma'].isin(b['lemma'])]
 assert a.shape[0] > 0, a.shape
 word_df = pd.concat((a, b))
 
-#   Filter out uncommon words and write to CSV (ordered by decreasing
-#   frequency)
-word_df = word_df[word_df['freq'] > FREQ_CUTOFF]
+#   Write to CSV (ordered by decreasing frequency)
+print(f'{word_df.shape[0]} words written to cleaned list.')
 word_df.sort_values('freq', ascending = False, inplace = True)
 word_df.to_csv(out_path, index = False, columns = ('lemma', 'freq'))
