1ae62931f71e60e6b5bde1a636d972e2f4d2f66c
Sculps <nickeagles77@gmail.com>
2022-08-16 19:28:59 -0400

    Add a function to compute game status and use it to correctly exit the game when done

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import pandas as pd
 import random
+import time
 
 import display
 
@@ -27,17 +28,15 @@ display_settings = {
     'keyboard_gap': 10
 }
 
-#   Modify the board given the current guess. Return 1 if there are no guesses
-#   left and 0 otherwise
-def next_guess(board, guess):
+#   Modify the board given the current guess. Return the game status: see
+#   'board_status'
+def next_guess(board, guess, word):
     empty_row = get_empty_row(board)
-    if empty_row == board.shape[0]:
-        return 1
-    else:
+    if empty_row < board.shape[0]:
         for i, x in enumerate(guess):
             board[empty_row, i] = guess[i]
-    
-        return 0
+        
+    return board_status(board, word)
 
 #   Return the index of the first empty row of board, or board.shape[0] if all
 #   rows are full
@@ -47,6 +46,26 @@ def get_empty_row(board):
         row += 1
 
     return row
+
+#   Given the board and correct (hidden) word, return -1 if all guesses have
+#   been exhausted without success, return 0 if the game is in progress, and
+#   return 1 if the player has correctly guessed the word
+def board_status(board, word):
+    empty_row = get_empty_row(board)
+
+    if empty_row == 0:
+        #   Totally empty board indicates game is in progress
+        return 0
+    elif ''.join(board[empty_row - 1, :].tolist()) == word:
+        #   If the last row contains the correct word, the player won
+        return 1
+    elif empty_row == board.shape[0]:
+        #   Otherwise if the board is full, the player lost
+        return -1
+    else:
+        #   In all other situations, the game is still going
+        return 0
+
 
 #   Initialize the screen
 screen, board = display.start(display_settings)
@@ -66,5 +85,8 @@ hidden_word = words.iloc[random.randint(0, words.shape[0] - 1)]['lemma']
 status = 0
 while status == 0:
     guess = input('Next guess:')
-    status = next_guess(board, guess)
+    status = next_guess(board, guess, hidden_word)
     display.display_board(board, screen, display_settings, hidden_word, get_empty_row(board))
+
+#   Show the final screen for a few seconds
+time.sleep(5)
