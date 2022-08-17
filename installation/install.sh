92c25cd5ee2a8ad63fd97a1dd6d62cc842e88813
Sculps <nickeagles77@gmail.com>
2022-08-16 20:44:35 -0400

    Provide an environment.yml file for the repo

--- installation/install.sh
+++ installation/install.sh
@@ -1,6 +1,4 @@
 #   Commands initially used to isolate python + dependencies while developing.
-#   When the project is further along, we'll provide an 'environment.yml' file
-#   and recommend creation of a conda environment.
 
 #   Create a basic environment with just python
 conda create -y -p $PWD/wordle_env python=3.8
